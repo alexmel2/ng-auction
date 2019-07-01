0da2cf89e96c1e82dc81f26580f2bed8b2afa08d
alex <alexmel2@gmail.com>
2019-07-01 14:28:53 +0300

    fun

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ng-auction'));
+app.use(express.static('./dist/ng-auction'));
 
 app.get('/*', function(req,res) {
     
